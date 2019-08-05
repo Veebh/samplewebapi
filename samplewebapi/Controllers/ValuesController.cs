d568c8203bf0bcb57b1bd15d89c7d34a0a3ac359
Chandra V <chandramouli.v@morganstanley.com>
2019-08-05 08:20:28 +0000

    cloned from original

--- samplewebapi/Controllers/ValuesController.cs
+++ samplewebapi/Controllers/ValuesController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Xml;
+using corewebapiwithswaggerui;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -18,13 +20,86 @@ namespace samplewebapi.Controllers
         //[Authorize(Roles = Role.Admin)]
         public ActionResult<string> Get()
         {
+            ClaimsPrincipal principal = HttpContext.User as ClaimsPrincipal;
             var keyvalue = new List<KeyValuePair<string, string>>();
+            if (null != principal)
+            {
+                if (principal.Claims == null)
+                {
+                    keyvalue.Add(new KeyValuePair<string, string>("principal.Claims", $"principal.Claims is null"));
+                }
+                else
+                {
+                    keyvalue.Add(new KeyValuePair<string, string>("principal.Claims.Count", principal.Claims.ToList<Claim>().Count.ToString()));
+
+                }
+                foreach (Claim claim in principal.Claims)
+                {
+                    keyvalue.Add(new KeyValuePair<string, string>(claim.Type, claim.Value.ToString()));
+                }
+            }
+            else
+            {
+                keyvalue.Add(new KeyValuePair<string, string>("missing claims", $"principle is null"));
+            }
+            if (HttpContext.User.Identity != null)
+            {
+                keyvalue.Add(new KeyValuePair<string, string>("HttpContext.User.Identity.AuthenticationType", HttpContext.User.Identity.AuthenticationType));
+                keyvalue.Add(new KeyValuePair<string, string>("HttpContext.User.Identity.IsAuthenticated", HttpContext.User.Identity.IsAuthenticated.ToString()));
+                keyvalue.Add(new KeyValuePair<string, string>("HttpContext.User.Identity.Name", HttpContext.User.Identity.Name));
+            }
+            try
+            {
+                Claim displayName = ClaimsPrincipal.Current?.FindFirst(ClaimsPrincipal.Current.Identities?.First()?.NameClaimType);
+                keyvalue.Add(new KeyValuePair<string, string>("displayName", displayName != null ? displayName.Value : string.Empty));
+            }
+            catch (Exception ex)
+            {
+                keyvalue.Add(new KeyValuePair<string, string>("corewebapiwithswaggerui-headers", ex.Message + ex.StackTrace));
+            }
+            //ControllerBase.User
+            //keyvalue.Add(new KeyValuePair<string, string>(" ControllerBase.User", ControllerBase.User.Claims);
+            foreach (Claim claim in this.User.Claims)
+            {
+                keyvalue.Add(new KeyValuePair<string, string>(" ControllerBase.User " + claim.Type, claim.Value.ToString()));
+            }
+            if (this.User.Identity != null)
+            {
+                keyvalue.Add(new KeyValuePair<string, string>(" ControllerBase.User HttpContext.User.Identity.AuthenticationType", this.User.Identity.AuthenticationType));
+                keyvalue.Add(new KeyValuePair<string, string>(" ControllerBase.User HttpContext.User.Identity.IsAuthenticated", this.User.Identity.IsAuthenticated.ToString()));
+                keyvalue.Add(new KeyValuePair<string, string>(" ControllerBase.User HttpContext.User.Identity.Name", this.User.Identity.Name));
+            }
+            keyvalue.Add(new KeyValuePair<string, string>("Enduser jwt header", "------------------------------------------"));
+            var jwtReader = CustomJwtValidation.CustomValidateJwt(HttpContext);
+            if (jwtReader == null)
+            {
+                keyvalue.Add(new KeyValuePair<string, string>("Request Header is null ", "------------------------------------------"));
+
+            }
+            else
+            {
+                foreach (var h in jwtReader.JwtInHeader)
+                {
+                    keyvalue.Add(new KeyValuePair<string, string>(h.Key, h.Value.ToString()));
+
+                }
+
+                keyvalue.Add(new KeyValuePair<string, string>("Enduser Clams", "------------------------------------------"));
+                foreach (var c in jwtReader.JwtCliams)
+                {
+                    keyvalue.Add(new KeyValuePair<string, string>(c.Type, c.Value));
+
+                }
+                keyvalue.Add(new KeyValuePair<string, string>("Request claims", "------------------------------------------"));
+            }
+
             foreach (var header in HttpContext.Request.Headers)
             {
                 var key = header.Key;
                 var val = header.Value;
                 keyvalue.Add(new KeyValuePair<string, string>(key, val));
             }
+            keyvalue.Add(new KeyValuePair<string, string>("appname", "corewebapiwithswaggerui"));
             return JsonConvert.SerializeObject(keyvalue, Newtonsoft.Json.Formatting.Indented).ToString();
         }
 
---
+++ samplewebapi/CustomJwtValidation.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace corewebapiwithswaggerui
+{
+    public class CustomJwtValidation
+    {
+        private readonly RequestDelegate _next;
+        static StringValues jwtAccessToken;
+
+        public CustomJwtValidation(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            CustomValidateJwt(httpContext);
+            return _next(httpContext);
+        }
+
+        public static JwtReader CustomValidateJwt(HttpContext httpContext)
+        {
+            //if (string.IsNullOrEmpty(jwtAccessToken))
+            {
+                string authorization = httpContext.Request.Headers["X-MS-EndUserAuthorization"];
+
+                // If no authorization header found, nothing to process further
+                if (string.IsNullOrEmpty(authorization))
+                {
+                    return null;
+                }
+
+                if (authorization.StartsWith("Bearer ", StringComparison.OrdinalIgnoreCase))
+                {
+                    jwtAccessToken = authorization.Substring("Bearer ".Length).Trim();
+                }
+
+                // If no token found, no further work possible
+                if (string.IsNullOrEmpty(jwtAccessToken))
+                {
+                    return null;
+                }
+
+                return new JwtReader(jwtAccessToken);
+            }            
+        }
+    }
+
+    public class JwtReader
+    {
+        static JwtSecurityTokenHandler jwtHandler = new JwtSecurityTokenHandler();
+        static JwtSecurityToken token = null;
+        public static JwtHeader JwtHeader;
+
+        public JwtReader(string jwtInText)
+        {
+
+            token = jwtHandler.ReadJwtToken(jwtInText);            
+        }
+
+        public JwtHeader JwtInHeader
+        {
+            get
+            {
+
+                //Extract the headers of the JWT
+                return token.Header;
+            }
+        }
+
+        public IEnumerable<Claim> JwtCliams
+        {
+            get
+            {
+                
+                //Extract the headers of the JWT
+                return token.Claims;
+            }
+        }
+    }
+}
